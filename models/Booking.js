ebba9a4d734c94ee532b35e8e05234e0156acc03
luthfi-izzuddin <luthfi.izzuddin1@gmail.com>
2022-09-07 21:10:04 +0700

    Create models Staycation

---
+++ models/Booking.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema;
+
+const bookingSchema = new mongoose.Schema({
+  bookingStartDate: {
+    type: date,
+    required: true,
+  },
+  bookingEndDate: {
+    type: date,
+    required: true,
+  },
+  itemId: [
+    {
+      _id: {
+        type: ObjectId,
+        ref: "Item",
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      night: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  memberId: [
+    {
+      type: ObjectId,
+      ref: "Member",
+    },
+  ],
+  bankId: [
+    {
+      type: ObjectId,
+      ref: "Bank",
+    },
+  ],
+  proofPayment: {
+    type: String,
+    required: true,
+  },
+  bankFrom: {
+    type: String,
+    required: true,
+  },
+  accountHolder: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
